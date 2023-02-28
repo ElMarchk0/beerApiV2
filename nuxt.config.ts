485bc837ecf03d978cfe964428c18acf6f3e21a9
EMarckh0 <petermc9011@gmail.com>
2023-02-27 20:17:48 -0800

    switching

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,10 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  runtimeConfig: {
+    mongoUrl: process.env.DATABASE_URL,
+  },
+  nitro: {
+    plugins: ["~/server/index.ts"],
+  },
 });
---
+++ server/api/beers/index.get.ts
@@ -0,0 +1,8 @@
+import { Beer } from "../../../db/models/beer.models";
+
+export default getBeerByQuery(async (query: string) => {
+  try {
+  } catch (e) {
+    console.error(e);
+  }
+});
